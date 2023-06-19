2fea9796a04ca8f16de3cf1c9cc7f077bb17a41d
sasha <voropaeva-sasha@mail.ru>
2023-06-20 00:07:21 +0300

    Task_3 added

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Enter the number that represents the day of the week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("This day is a holyday");
+    }
+    else if (dayNumber < 1 || dayNumber >7)
+    {
+        Console.WriteLine("There is no such day");
+    }    
+    else
+    {Console.WriteLine("There is no holiday");}
+}
+CheckingTheDayOfTheWeek (dayNumber);
+    
